ca9ee7ddc7b4b2ef37c527cc3cfa64ffbf9a572b
Nonepenguin <pendragon707@mail.ru>
2025-01-29 10:16:26 +0300

    change file names

--- src/policy_aliengo.py
+++ src/policy_aliengo.py
@@ -39,6 +39,14 @@ HIGHLEVEL = 0x00
 LOWLEVEL  = 0xff
 
 
+def setup_motor(cmd, num_motor, q, dq, Kp, Kd, tau):
+    cmd.motorCmd[num_motor].q = q
+    cmd.motorCmd[num_motor].dq = dq
+    cmd.motorCmd[num_motor].Kp = Kp
+    cmd.motorCmd[num_motor].Kd = Kd
+    cmd.motorCmd[num_motor].tau = tau
+
+
 def to_observation(state, action_history):
     obs = []
     imu_quaternion = state.imu.quaternion
@@ -84,10 +92,10 @@ def main(args):
 
     udp = sdk.UDP(LOCAL_PORT, TARGET_IP, TARGET_PORT, LOW_CMD_LENGTH, LOW_STATE_LENGTH, -1)
     
-    lcmd = sdk.LowCmd()
+    cmd = sdk.LowCmd()
     state = sdk.LowState()
     udp.InitCmdData(cmd)
-    lcmd.levelFlag = LOWLEVEL
+    cmd.levelFlag = LOWLEVEL
 
     prop_loaded_encoder = torch.jit.load(prop_enc_pth).to(device)
     loaded_mlp = torch.jit.load(mlp_pth).to(device)
@@ -151,12 +159,15 @@ def main(args):
             command = command.Command(q=action, Kp=[Kp]*12, Kd=[Kd]*12)
             # cmd.clamp_q()
 
-            for i in range(12):
-                cmd.motorCmd[i].q = command.get_command(i)[0]
-                cmd.motorCmd[i].dq = command.get_command(i)[1]
-                cmd.motorCmd[i].Kp = command.get_command(i)[2]
-                cmd.motorCmd[i].Kd = command.get_command(i)[3]
-                cmd.motorCmd[i].tau = command.get_command(i)[4]
+            # for i in range(12):
+            #     cmd.motorCmd[i].q = command.get_command(i)[0]
+            #     cmd.motorCmd[i].dq = command.get_command(i)[1]
+            #     cmd.motorCmd[i].Kp = command.get_command(i)[2]
+            #     cmd.motorCmd[i].Kd = command.get_command(i)[3]
+            #     cmd.motorCmd[i].tau = command.get_command(i)[4]
+
+            for i in range( 12 ):
+                setup_motor(cmd, i, *command.get_command(i))
 
             udp.SetSend( cmd )
             udp.Send() 
