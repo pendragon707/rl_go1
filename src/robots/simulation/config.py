e6a48354ea330b1ecab5ec0e024aef330c441b62
nonpenguin <none@penguin.com>
2025-09-23 21:09:29 +0300

    add model param to policy script

--- src/robots/simulation/config.py
+++ src/robots/simulation/config.py
@@ -1,5 +1,5 @@
 # ROBOT = "go1" # Robot name, "go2", "b2", "b2w", "h1", "go2w", "g1" 
-ROBOT = "go1"
+ROBOT = "aliengo"
 ROBOT_SCENE = "submodules/unitree_mujoco/unitree_robots/" + ROBOT + "/scene.xml" # Robot scene
 DOMAIN_ID = 1 # Domain id
 
